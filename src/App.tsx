162e732006a79152e9a47122ab6474f23c562825
norland <nikitafoka30@gmail.con>
2024-02-26 16:41:06 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import './App.css'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import Layout from "./pages/Layout/Layout.tsx";
 import ListsPage from "./pages/ListsPage/ListsPage.tsx";
-import {todosLoader} from "./loaders/todosLoader.ts";
 
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout/>}>
-      <Route path="/" element={<ListsPage />} loader={todosLoader}/>
+      <Route path="/" element={<ListsPage />}/>
     </Route>
   ))
 
